8fcaeea431de33c4dbcb8a005c1ef29cd6106290
ninatran <ninatran.az@gmail.com>
2022-07-11 01:03:27 -0500

    remove whitespace and clean up code

--- script.js
+++ script.js
@@ -2,66 +2,56 @@ const base_url = "https://api.parsely.com/v2/";
 const authentication_params = new URLSearchParams(config);
 
 // Construct endpoint to get top 5 authors in the past 24 hours by social interactions
-const top_authors_params = {
+let search_params = {
     period_start: '24h',
     sort: 'social_interactions',
     limit: 5,
 }
 const top_authors_path = "analytics/authors";
-const top_authors_endpoint = endpoint(top_authors_path, top_authors_params);
+const top_authors_endpoint = endpoint(top_authors_path, search_params);
 
 // Fetch trending authors from API
 fetch(top_authors_endpoint)
-.then(res => res.json())
+.then(response => response.json())
 .then(response => { 
-    let top_five_authors = [];
-    response.data.forEach( element => {
-        top_five_authors.push({
-            author: element.author,
-            social_interactions: element.metrics.social_interactions
-        });
+    // Extract author name and metrics from response
+    let top_authors = [];
+    response.data.forEach( element => { 
+        top_authors.push({author: element.author, social_interactions: element.metrics.social_interactions})
     });
+    // Generate endpoint for top article of each author
+    search_params.sort = 'fb_referrals';
+    search_params.limit = 1;
     const author_detail_path = (author) => "analytics/author/" + author.replace(/ /g,'%20') + "/detail";
-    const author_detail_params = {
-        period_start: '24h',
-        sort: 'fb_referrals',
-        limit: 1
-    }
-
-    top_five_authors.forEach(element => {
-        element.endpoint = endpoint(author_detail_path(element.author), author_detail_params);
+    top_authors.forEach(element => {
+        element.endpoint = endpoint(author_detail_path(element.author), search_params);
     })
-
-    return top_five_authors;
+    return top_authors;
  })
- .then((author_details) => {
+ .then(top_authors => {
     // Get top article from Facebook referrals for each author
     Promise.all([
-        fetch(author_details[0].endpoint),
-        fetch(author_details[1].endpoint),
-        fetch(author_details[2].endpoint),
-        fetch(author_details[3].endpoint),
-        fetch(author_details[4].endpoint),
-    ]).then((res) => {
-        return Promise.all(res.map(response => response.json()));
+        fetch(top_authors[0].endpoint),
+        fetch(top_authors[1].endpoint),
+        fetch(top_authors[2].endpoint),
+        fetch(top_authors[3].endpoint),
+        fetch(top_authors[4].endpoint),
+    ]).then(response => {
+        return Promise.all(response.map(response => response.json()));
     }).then(responses => {
         let top_articles = [];
         responses.forEach(response => top_articles.push(response.data.pop()));
-        return top_articles; //array of objects each object is the top post from each author
-    })
-    .then( top_articles => {
-        display(top_articles, author_details);
+        //Render
+        display(top_articles, top_authors);
     })
     .catch(err => {console.error(err)});
  })
 .catch(err => {console.error(err)})
 
-// Populates HTML page with data from API
+// Displays data on HTML Page
 function display(articleDetails, authorDetails){
     const container = document.querySelector('#results-container');
-    
-    for(let i = 0; i < articleDetails.length; i++){
-        
+    for(let i = 0; i < articleDetails.length; i++){ 
         const content = `<section class="author-listing">
             <header>
                 <h1>${i+1}. ${articleDetails[i].author}</h1>
@@ -75,10 +65,8 @@ function display(articleDetails, authorDetails){
                 </div>
             </article>
         </section>`;
-        
         container.insertAdjacentHTML('beforeend', content);
     }
-
 }
 
 // Returns endpoint with search params and authentication
